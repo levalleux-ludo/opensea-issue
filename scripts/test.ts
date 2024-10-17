e3150f01a001effdba00e9f3508b645dc0fb952f
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2024-10-17 15:01:37 +0100

    complete node test implementation

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,5 +1,39 @@
+import { Chain, OpenSeaSDK, OrderSide } from "opensea-js";
+import { JsonRpcProvider } from "ethers";
+
+const TOKEN_ID="1729162321012";
+const NFT_CONTRACT="0x71ebc24b00f65d6cd5848cef5971595dab6027f9";
+const JSON_RPC="https://rpc-amoy.polygon.technology/"
+
 async function main() {
-  console.log("Hello World");
+  const provider = new JsonRpcProvider(JSON_RPC);
+  const fulfillerAddress = "0xC2E8F052924CA9432F0a416a1b9f9DAedabaEBF2";
+  const openseaSdk = new OpenSeaSDK(
+    provider,
+    { chain: Chain.Amoy }
+  );
+  const osOrder = await openseaSdk.api.getOrder({
+    assetContractAddress: NFT_CONTRACT,
+    tokenId: TOKEN_ID,
+    side: OrderSide.OFFER
+  });
+  console.log({
+    fulfillerAddress,
+    osOrder: {
+      orderHash: osOrder.orderHash,
+      protocolAddress: osOrder.protocolAddress,
+      side: osOrder.side
+    }
+  });
+  if (osOrder) {
+    const ffd = await openseaSdk.api.generateFulfillmentData(
+      fulfillerAddress,
+      osOrder.orderHash || "",
+      osOrder.protocolAddress,
+      osOrder.side
+    );
+    console.log({ ffd });  
+  }
 };
 
 main().then(() => {
